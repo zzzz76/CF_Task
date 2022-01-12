c8e611939fa74ea1ac32e6fac704640dd749b131
zzzz76 <15079084006@163.com>
2022-01-12 13:21:56 +0800

    add utils class

--- lab/mf_general.py
+++ lab/mf_general.py
@@ -3,96 +3,30 @@ LFM Model
 '''
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-
-
-def accuray(predict_results, method="all"):
-    '''
-    准确性指标计算方法
-    :param predict_results: 预测结果，类型为容器，每个元素是一个包含uid,iid,real_rating,pred_rating的序列
-    :param method: 指标方法，类型为字符串，rmse或mae，否则返回两者rmse和mae
-    :return:
-    '''
-
-    def rmse(predict_results):
-        '''
-        rmse评估指标
-        :param predict_results:
-        :return: rmse
-        '''
-        length = 0
-        _rmse_sum = 0
-        for uid, iid, real_rating, pred_rating in predict_results:
-            length += 1
-            _rmse_sum += (pred_rating - real_rating) ** 2
-        return round(np.sqrt(_rmse_sum / length), 4)
-
-    def mae(predict_results):
-        '''
-        mae评估指标
-        :param predict_results:
-        :return: mae
-        '''
-        length = 0
-        _mae_sum = 0
-        for uid, iid, real_rating, pred_rating in predict_results:
-            length += 1
-            _mae_sum += abs(pred_rating - real_rating)
-        return round(_mae_sum / length, 4)
-
-    def rmse_mae(predict_results):
-        '''
-        rmse和mae评估指标
-        :param predict_results:
-        :return: rmse, mae
-        '''
-        length = 0
-        _rmse_sum = 0
-        _mae_sum = 0
-        for uid, iid, real_rating, pred_rating in predict_results:
-            length += 1
-            _rmse_sum += (pred_rating - real_rating) ** 2
-            _mae_sum += abs(pred_rating - real_rating)
-        return round(np.sqrt(_rmse_sum / length), 4), round(_mae_sum / length, 4)
-
-    if method.lower() == "rmse":
-        return rmse(predict_results)
-    elif method.lower() == "mae":
-        return mae(predict_results)
-    else:
-        return rmse_mae(predict_results)
-
-def curve(costs):
-    n = len(costs)
-    x = range(n)
-    plt.plot(x, costs, color='r', linewidth=3)
-    plt.title("Convergence curve")
-    plt.xlabel("generation")
-    plt.ylabel("loss")
-    plt.show()
+from lab.utils import accuray, curve
 
 # 评分预测    1-5
 class LFM(object):
 
-    def __init__(self, alpha, reg_p, reg_q, number_LatentFactors=10, number_epochs=10,
+    def __init__(self, alpha, reg_u, reg_w, number_LatentFactors=10, number_epochs=10,
                  columns=["uid", "iid", "rating"]):
         self.alpha = alpha  # 学习率
-        self.reg_p = reg_p  # P矩阵正则
-        self.reg_q = reg_q  # Q矩阵正则
+        self.reg_u = reg_u  # P矩阵正则
+        self.reg_w = reg_w  # Q矩阵正则
         self.number_LatentFactors = number_LatentFactors  # 隐式类别数量
         self.number_epochs = number_epochs  # 最大迭代次数
         self.columns = columns
 
-    def fit(self, dataset, validset):
-        self.dataset = pd.DataFrame(dataset)
+    def fit(self, trainset, validset):
+        self.trainset = pd.DataFrame(trainset)
         self.validset = pd.DataFrame(validset)
 
-        self.users_ratings = dataset.groupby(self.columns[0]).agg([list])[[self.columns[1], self.columns[2]]]
-        self.items_ratings = dataset.groupby(self.columns[1]).agg([list])[[self.columns[0], self.columns[2]]]
+        self.users_ratings = trainset.groupby(self.columns[0]).agg([list])[[self.columns[1], self.columns[2]]]
+        self.items_ratings = trainset.groupby(self.columns[1]).agg([list])[[self.columns[0], self.columns[2]]]
 
-        self.globalMean = self.dataset[self.columns[2]].mean()
+        self.globalMean = self.trainset[self.columns[2]].mean()
 
-        self.P, self.Q = self.train()
+        self.U, self.W = self.train()
 
     def train(self):
         """
@@ -105,17 +39,15 @@ class LFM(object):
         vr_min = 10
         vr_last = 0
         costs = []
-        P, Q = self._init_matrix()
+        U, W = self._init_matrix() # 模型初始化
         for i in range(self.number_epochs):
             print("==========  epoch %d ==========" % i)
-            P, Q = self.sgd(P, Q)
-            cost = self.cost(P, Q)
+            U, W = self.sgd(U, W) # 每一轮更新 都要 计算 cost
+            cost = self.cost(U, W)
             print("Training cost: ", cost)
             costs.append(cost)
-            if cost < 0.01:
-                break
 
-            valid_results = lfm.valid(P, Q)
+            valid_results = self.valid(U, W)
             rmse = accuray(valid_results, method="rmse")
             print("Validation rmse: ", rmse)
 
@@ -127,8 +59,8 @@ class LFM(object):
             else:
                 break
 
-        curve(costs)
-        return P, Q
+        curve(costs) # 对每一轮的cost 绘制 收敛图
+        return U, W
 
 
     def _init_matrix(self):
@@ -137,66 +69,66 @@ class LFM(object):
         :return: 隐空间矩阵
         """
         # User-LF
-        P = dict(zip(
+        U = dict(zip(
             self.users_ratings.index,
             np.random.rand(len(self.users_ratings), self.number_LatentFactors).astype(np.float32)
         ))
         # Item-LF
-        Q = dict(zip(
+        W = dict(zip(
             self.items_ratings.index,
             np.random.rand(len(self.items_ratings), self.number_LatentFactors).astype(np.float32)
         ))
-        return P, Q
+        return U, W
 
-    def sgd(self, P, Q):
+    def sgd(self, U, W):
         """
         使用随机梯度下降，优化模型
-        :param P: 用户隐空间矩阵
-        :param Q: 服务隐空间矩阵
+        :param U: 用户隐空间矩阵
+        :param W: 服务隐空间矩阵
         :return: 经过优化的隐空间矩阵
         """
-        for uid, iid, r_ui in self.dataset.itertuples(index=False):
-            # User-LF P
-            ## Item-LF Q
-            v_pu = P[uid]  # 用户向量
-            v_qi = Q[iid]  # 物品向量
-            err = np.float32(r_ui - np.dot(v_pu, v_qi))
+        for uid, iid, r_ui in self.trainset.itertuples(index=False):
+            # User-LF U
+            ## Item-LF W
+            v_u = U[uid]  # 用户向量
+            v_i = W[iid]  # 物品向量
+            err = np.float32(r_ui - np.dot(v_u, v_i))
 
-            v_pu += self.alpha * (err * v_qi - self.reg_p * v_pu)
-            v_qi += self.alpha * (err * v_pu - self.reg_q * v_qi)
+            v_u += self.alpha * (err * v_i - self.reg_u * v_u)
+            v_i += self.alpha * (err * v_u - self.reg_w * v_i)
 
-            P[uid] = v_pu
-            Q[iid] = v_qi
-        return P, Q
+            U[uid] = v_u
+            W[iid] = v_i
+        return U, W
 
-    def cost(self, P, Q):
+    def cost(self, U, W):
         """
         计算损失值
-        :param P: 用户隐空间矩阵
-        :param Q: 服务隐空间矩阵
+        :param U: 用户隐空间矩阵
+        :param W: 服务隐空间矩阵
         :return: 模型损失值
         """
         cost = 0
-        for uid, iid, r_ui in self.dataset.itertuples(index=False):
-            v_pu = P[uid]  # 用户向量
-            v_qi = Q[iid]  # 物品向量
-            cost += pow(r_ui - np.dot(v_pu, v_qi), 2)
+        for uid, iid, r_ui in self.trainset.itertuples(index=False):
+            v_u = U[uid]  # 用户向量
+            v_i = W[iid]  # 物品向量
+            cost += pow(r_ui - np.dot(v_u, v_i), 2)
 
             for k in range(self.number_LatentFactors):
-                cost += self.reg_p * pow(v_pu[k], 2) + self.reg_q * pow(v_qi[k], 2)
+                cost += self.reg_u * pow(v_u[k], 2) + self.reg_w * pow(v_i[k], 2)
 
         return cost / 2
 
-    def valid(self, P, Q):
+    def valid(self, U, W):
         """
         验证数据集
-        :param P: 用户隐空间矩阵
-        :param Q: 服务隐空间矩阵
+        :param U: 用户隐空间矩阵
+        :param W: 服务隐空间矩阵
         :return: 逐个返回验证评分
         """
         for uid, iid, real_rating in self.validset.itertuples(index=False):
             try:
-                pred_rating = np.dot(P[uid], Q[iid])
+                pred_rating = np.dot(U[uid], W[iid])
             except Exception as e:
                 print(e)
             else:
@@ -212,10 +144,10 @@ class LFM(object):
         if uid not in self.users_ratings.index or iid not in self.items_ratings.index:
             return self.globalMean
 
-        p_u = self.P[uid]
-        q_i = self.Q[iid]
+        v_u = self.U[uid]
+        v_i = self.W[iid]
 
-        return np.dot(p_u, q_i)
+        return np.dot(v_u, v_i)
 
     def test(self, testset):
         """
---
+++ lab/utils.py
@@ -0,0 +1,72 @@
+"""
+To provide the evaluation utils
+
+@author zzzz76
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def accuray(predict_results, method="all"):
+    '''
+    准确性指标计算方法
+    :param predict_results: 预测结果，类型为容器，每个元素是一个包含uid,iid,real_rating,pred_rating的序列
+    :param method: 指标方法，类型为字符串，rmse或mae，否则返回两者rmse和mae
+    :return:
+    '''
+
+    def rmse(predict_results):
+        '''
+        rmse评估指标
+        :param predict_results:
+        :return: rmse
+        '''
+        length = 0
+        _rmse_sum = 0
+        for uid, iid, real_rating, pred_rating in predict_results:
+            length += 1
+            _rmse_sum += (pred_rating - real_rating) ** 2
+        return round(np.sqrt(_rmse_sum / length), 4)
+
+    def mae(predict_results):
+        '''
+        mae评估指标
+        :param predict_results:
+        :return: mae
+        '''
+        length = 0
+        _mae_sum = 0
+        for uid, iid, real_rating, pred_rating in predict_results:
+            length += 1
+            _mae_sum += abs(pred_rating - real_rating)
+        return round(_mae_sum / length, 4)
+
+    def rmse_mae(predict_results):
+        '''
+        rmse和mae评估指标
+        :param predict_results:
+        :return: rmse, mae
+        '''
+        length = 0
+        _rmse_sum = 0
+        _mae_sum = 0
+        for uid, iid, real_rating, pred_rating in predict_results:
+            length += 1
+            _rmse_sum += (pred_rating - real_rating) ** 2
+            _mae_sum += abs(pred_rating - real_rating)
+        return round(np.sqrt(_rmse_sum / length), 4), round(_mae_sum / length, 4)
+
+    if method.lower() == "rmse":
+        return rmse(predict_results)
+    elif method.lower() == "mae":
+        return mae(predict_results)
+    else:
+        return rmse_mae(predict_results)
+
+def curve(costs):
+    x = range(len(costs))
+    plt.plot(x, costs, color='r', linewidth=3)
+    plt.title("Convergence curve")
+    plt.xlabel("generation")
+    plt.ylabel("loss")
+    plt.show()
